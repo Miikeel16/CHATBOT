e835609553476313ebb468264c648345c8b89928
Ieltxu Albin <ieltxu.albin02@somo.eus>
2025-04-02 10:57:10 +0200

    Añadiendo comentarios para el guardado del historial

--- src/app/chatbot/components/dashboard/dashboard.component.ts
+++ src/app/chatbot/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, Input, input, signal, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, Input, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { map } from 'rxjs';
 import type { Chats } from '../../interfaces/mensaje.interface';
@@ -9,18 +9,20 @@ import type { Chats } from '../../interfaces/mensaje.interface';
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
+  // Inyección de dependencias para el enrutador y la ruta activada
   private router = inject(Router);
   private activatedRoute = inject(ActivatedRoute);
 
   // Referencia al campo de entrada para el nuevo chat
   @ViewChild('txtnew') txtnew!: ElementRef;
+  // Se le pasa la lista de chats
   @Input() chats: Chats[] = [];
   // Señal que indica si hay un error en la entrada
   inputError = signal(false);
 
   ngOnChanges() {
     // Verifica los parámetros de la consulta cada vez que cambien los chats
-    this.checkQueryParam();
+    this.checkRouteExist();
   }
 
   /**
@@ -38,9 +40,11 @@ export class DashboardComponent {
       return;
     }
 
+    // Crea un nuevo chat y lo agrega a la lista
     const newChat: Chats = { titulo: chatTitle, mensajes: [] };
     this.chats.push(newChat);
-    localStorage.setItem('chatCompleteHistory', JSON.stringify(this.chats));
+    // Guarda en localStorage
+    localStorage.setItem('chatHistory', JSON.stringify(this.chats));
     // Limpia el campo de entrada
     this.txtnew.nativeElement.value = '';
   }
@@ -50,20 +54,30 @@ export class DashboardComponent {
    * @param index - El índice del chat a eliminar.
    */
   eliminarChat(index: number) {
+    // Elimina el chat del array
     this.chats.splice(index, 1);
-    localStorage.setItem('chatCompleteHistory', JSON.stringify(this.chats));
-    this.checkQueryParam();
-  }
-  eliminarTodosLosChats() {
-    this.chats = [];  // Vacía el array de chats
-    localStorage.removeItem('chatCompleteHistory'); // Elimina el historial almacenado
-    this.checkQueryParam();
+    // Actualiza localStorage
+    localStorage.setItem('chatHistory', JSON.stringify(this.chats));
+    // Verifica que exista la ruta en el chatHistory
+    this.checkRouteExist();
   }
 
-  private checkQueryParam() {
+  // Elimina todos los chats del historial
+  eliminarTodosLosChats() {
+    // Vacía el array de chats
+    this.chats = [];
+    // Elimina el historial almacenado
+    localStorage.removeItem('chatHistory');
+    // Verifica que exista la ruta en el chatHistory
+    this.checkRouteExist();
+  }
+
+  // Verifica que exista la ruta en el chatHistory
+  private checkRouteExist() {
     this.activatedRoute.params
-      .pipe(map(params => params['query']))
+      .pipe(map(params => params['query'])) // Mapea los parámetros de la ruta
       .subscribe(query => {
+        // Redirige a raiz si el query no coincide con ningún chat
         if (query && !this.chats.some(chat => chat.titulo === query)) {
           this.router.navigate(['/']);
         }
--- src/app/chatbot/components/question-bar/question-bar.component.ts
+++ src/app/chatbot/components/question-bar/question-bar.component.ts
@@ -19,16 +19,19 @@ export class QuestionBarComponent {
   chatbot = inject(ChatbotService);
   // Señal que almacena la cadena de mensajes del chat
   chainChat = signal<Mensajes[]>([]);
-
+  // Señal para indicar si se está cargando
   isLoading = signal<boolean>(false);
+  // Señal que almacena el historial de chats
   chat = signal<Chats[]>([]);
+
+  // Obtiene el parámetro 'query' de la ruta activa
   query = toSignal(
     inject(ActivatedRoute).params.pipe(map(params => params['query']))
   );
 
   constructor() {
     // Recupera el historial de chats del almacenamiento local
-    const stored = JSON.parse(localStorage.getItem('chatCompleteHistory') || '[]');
+    const stored = JSON.parse(localStorage.getItem('chatHistory') || '[]');
     // Si se recupera un array, lo establece en la señal de chats
     if (Array.isArray(stored)) {
       this.chat.set(stored);
@@ -40,10 +43,12 @@ export class QuestionBarComponent {
    * @param mensaje - El mensaje que se enviará a la API.
    */
   enviarMensaje(mensaje: string) {
+    // Evita enviar una petición si ya se está cargando
     if (this.isLoading()) return;
 
     // Verifica que el mensaje no esté vacío
     if (mensaje.trim() != '') {
+      // Activa la señal de carga
       this.isLoading.set(true);
       // Limpia el campo de entrada
       this.txtChat.nativeElement.value = '';
@@ -53,6 +58,7 @@ export class QuestionBarComponent {
         ...info,
         { texto: mensaje, tipo: 'usuario' } // Agrega el mensaje del usuario
       ])
+      // Guarda el mensaje en el historial
       this.guardarChat(mensaje, 'usuario');
 
       // Envía el mensaje al chatbot y maneja la respuesta
@@ -68,7 +74,9 @@ export class QuestionBarComponent {
             { texto: jsonArray.answare, tipo: 'bot' } // Agrega la respuesta de la API
           ])
 
+          // Guarda la respuesta en el historial
           this.guardarChat(jsonArray.answare, 'bot');
+          // Desactiva la señal de carga
           this.isLoading.set(false);
         }, error: (err) => {
           // Manejo de errores: agrega un mensaje de error a la cadena de chat
@@ -77,33 +85,52 @@ export class QuestionBarComponent {
             { texto: 'Error', tipo: 'bot' } // Mensaje de errpr
           ])
 
+          // Guarda la respuesta en el historial
           this.guardarChat('Error', 'bot');
+          // Desactiva la señal de carga
           this.isLoading.set(false);
         }
       });
     }
   }
 
+  /**
+   * Guarda el chat en el historial.
+   * @param contenido - Contenido del mensaje.
+   * @param tipoUser - Tipo de usuario ('usuario' o 'bot').
+   */
   guardarChat(contenido: string, tipoUser: 'usuario' | 'bot') {
     this.chat.update(data => {
+      // Obtiene el título del chat
       const nuevoTitulo = this.query() ? this.query() : null;
+      // Busca el índice del chat
       const index = data.findIndex(item => item.titulo === nuevoTitulo);
+
+      // Si el chat ya existe, agrega el nuevo mensaje
       if (index !== -1) {
         data[index].mensajes.push({ texto: contenido, tipo: tipoUser });
       } else {
+        // Si no existe, crea un nuevo chat
         data.push({
           titulo: nuevoTitulo,
           mensajes: [{ texto: contenido, tipo: tipoUser }]
         });
       }
+
+      // Devuelve el nuevo estado del historial
       return data;
     })
+
+    // Guarda el historial en localStorage si hay un título
     if (this.query() !== undefined) {
-      localStorage.setItem('chatCompleteHistory', JSON.stringify(this.chat()));
+      localStorage.setItem('chatHistory', JSON.stringify(this.chat()));
     }
-    const chatHistory = JSON.parse(localStorage.getItem('chatCompleteHistory') || '[]');
+
+    // Filtra el historial para eliminar entradas con título nulo
+    const chatHistory = JSON.parse(localStorage.getItem('chatHistory') || '[]');
     const filteredHistory = chatHistory.filter((item: { titulo: string | null; }) => item.titulo !== null);
-    localStorage.setItem('chatCompleteHistory', JSON.stringify(filteredHistory));
-    console.log(this.chat());
+
+    // Actualiza localStorage
+    localStorage.setItem('chatHistory', JSON.stringify(filteredHistory));
   }
 }
